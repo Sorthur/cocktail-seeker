c775497331bfc8be826f6ecfd65917f956048c08
Mateusz Dejewski <matdejewski@gmail.com>
2023-05-07 17:47:50 +0200

    add api constants

---
+++ lib/configuration/api_constants.dart
@@ -0,0 +1,9 @@
+class ApiConstants {
+  static String baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1';
+  static String cocktailEndpoint = '/lookup.php?i=';
+  // static String ingredientEndpoint = '/lookup.php?iid=';
+  static String categoryListEndpoint = '/list.php?c=list';
+  static String glassListEndpoint = '/list.php?g=list';
+  static String ingredientListEndpoint = '/list.php?i=list';
+  static String alcoholicListEndpoint = '/list.php?a=list';
+}
