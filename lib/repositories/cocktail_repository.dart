f3245e2a4335a67dd4d601a6a9c1d3632da08911
Mateusz Dejewski <matdejewski@gmail.com>
2023-05-09 21:15:50 +0200

    remove test methods

--- lib/repositories/cocktail_repository.dart
+++ lib/repositories/cocktail_repository.dart
@@ -4,34 +4,4 @@ import 'package:http/http.dart' as http;
 
 import '../models/filters/category_filter.dart';
 
-class CocktailRepository {
-  final String apiUrl;
-  T? cast<T>(x) => x is T ? x : null;
-
-  CocktailRepository(this.apiUrl);
-
-  /// Only sample method for test purposes
-  Future<List<CategoryFilter>> fetchDrinkCategories() async {
-    final response = await http.get(Uri.parse(apiUrl));
-
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body)['drinks'];
-
-      return data.map((json) => CategoryFilter.fromJson(json)).toList();
-    } else {
-      throw Exception('Failed to load drink categories');
-    }
-  }
-
-  /// Only sample method for test purposes
-  Future<DetailedCocktail> fetchDetailedCocktail() async {
-    final response = await http.get(Uri.parse(apiUrl));
-
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body)['drinks'];
-      return data.map((json) => DetailedCocktail.fromJson(json)).first;
-    } else {
-      throw Exception('Failed to load detailed cocktail');
-    }
-  }
-}
+class CocktailRepository {}
