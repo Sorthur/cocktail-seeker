daf1bde303ca97ecf374d1e35fedeacfd4a1819e
Mateusz Dejewski <matdejewski@gmail.com>
2023-05-09 21:23:47 +0200

    add empty cocktailRepo methods

--- lib/repositories/cocktail_repository.dart
+++ lib/repositories/cocktail_repository.dart
@@ -1,7 +1,11 @@
-import 'dart:convert';
-import 'package:cocktail_seeker/models/detailed_cocktail.dart';
+import 'package:cocktail_seeker/models/cocktail.dart';
 import 'package:http/http.dart' as http;
 
-import '../models/filters/category_filter.dart';
+import '../models/filters/filter.dart';
 
-class CocktailRepository {}
+class CocktailRepository {
+  Future<List<Cocktail>> getCocktailsByFilter(Filter filter) async =>
+      throw Exception('Not implemented');
+  Future<Cocktail> getCocktailsById(int id) async =>
+      throw Exception('Not implemented');
+}
